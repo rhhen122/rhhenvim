ee0c6ebdcbe44acd19eab46bac6baeb1745a815c
rhhen122 <rokyhenderson222@gmail.com>
2025-06-24 12:53:27 +1200

    First Commit

---
+++ lua/plugins/rose-pine.lua
@@ -0,0 +1,15 @@
+-- lua/plugins/rose-pine.lua
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    vim.cmd("colorscheme rose-pine")
+  end,
+  opt = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      float = "transparent",
+    },
+  },
+}
