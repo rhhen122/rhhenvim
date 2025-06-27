db4077c3a369011bbf6c42b54d04ed548d173810
rhhen122 <rokyhenderson222@gmail.com>
2025-06-27 14:05:42 +1200

    First Commit of RhhenVim Recode

--- lua/plugins/general.lua
+++
@@ -1,3 +0,0 @@
-return {
-    {'Konfekt/vim-alias'}
-}
---
+++ lua/rhhenvim/packer.lua
@@ -0,0 +1,86 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use({
+        "rose-pine/neovim",
+        as = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    })
+
+    use({
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    })
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    -- use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+
+    use "b0o/incline.nvim"
+
+    use ( "nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
+
+    use { 'neovim/nvim-lspconfig',
+        config = function()
+            require("lspconfig").ruff.setup{}
+            require'lspconfig'.lua_ls.setup{}
+            require 'lspconfig'.bashls.setup{}
+       end
+    }
+
+    -- VimBeGood
+    use 'ThePrimeagen/vim-be-good'
+
+    -- Golf.Vim
+    use 'vuciv/golf'
+
+    -- LSP 0
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+
+    use {
+        "folke/noice.nvim",
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    }
+
+
+end)
