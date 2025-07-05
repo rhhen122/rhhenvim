4e375c704ae2d16a4764439cd23692399e2c1a74
rhhen122 <rokyhenderson222@gmail.com>
2025-07-05 16:47:07 +1200

    Removed Error message

--- lua/rhhenvim/packer.lua
+++ lua/rhhenvim/packer.lua
@@ -61,7 +61,6 @@ return require('packer').startup(function(use)
     use 'dikiaap/minimalist'
 
 
-    use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
     use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
     use 'romgrk/barbar.nvim'
 
